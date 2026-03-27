d9d7ef3c21d8117702c40c93321e43ad35aeaae4
abhijitreddy-06 <abhijitreddy828@gmail.com>
2026-03-27 13:24:23 +0530

    seo

--- src/lib/seo.ts
+++ src/lib/seo.ts
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 
-const FALLBACK_SITE_URL = "https://telehealthx.com";
+const FALLBACK_SITE_URL = "https://telehealthx.vercel.app";
 
 export const siteUrl = (process.env.NEXT_PUBLIC_SITE_URL || FALLBACK_SITE_URL).replace(/\/$/, "");
 export const siteName = "TeleHealthx";
